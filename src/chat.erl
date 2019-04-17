cc21199a4adf73dbfa1e8bc036d156a33a397660
Vlad Ki <vlad@kirillov.im>
2019-04-17 09:16:17 +0000

    redirect /

--- src/chat.erl
+++ src/chat.erl
@@ -7,12 +7,25 @@
 -include_lib("kvx/include/metainfo.hrl").
 -compile(export_all).
 
+init(Req, Opts) ->
+    Body = <<"<script>window.location = 'https://chat.n2o.space';</script> Reticulating splines...">>,
+    {ok, cowboy_req:reply(200, #{}, [Body, "\n"], Req), Opts}.
+
+endpoints() ->
+    Static = {dir, n2o_cowboy:fix1(code:priv_dir(application:get_env(n2o,app,chat)))++"/static", []},
+    cowboy_router:compile([{'_', [
+        {"/", ?MODULE, []},
+        {"/ws/[...]", n2o_cowboy2, []},
+        {"/n2o/[...]", cowboy_static, {dir, n2o_cowboy:fix2(code:priv_dir(n2o)), []}},
+        {"/app/[...]", cowboy_static, Static},
+        {"/[...]", cowboy_static, Static}]}]).
+
 stop(_)    -> ok.
 start()    -> start(normal,[]).
 start(_,_) -> X = supervisor:start_link({local,?MODULE},?MODULE,[]),
               syn:init(),
               kvx:join(),
-              cowboy:start_tls(http,n2o_cowboy:env(?MODULE),#{env=>#{dispatch=>points()}}),
+              cowboy:start_tls(http,n2o_cowboy:env(?MODULE),#{env=>#{dispatch=>endpoints()}}),
               [ n2o_pi:start(#pi{module=n2o_wsnode,table=ring,
                                  sup=?MODULE,state=[],name={server,Pos}})
                 || {{_,_},Pos} <- lists:zip(n2o:ring(),lists:seq(1,length(n2o:ring()))) ],
