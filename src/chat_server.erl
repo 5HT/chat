e85645e2f862821fc2c43ed1419b1298f3bf0c42
Namdak Tonpa <maxim@synrc.com>
2019-04-17 22:57:00 +0300

    Update chat_server.erl

--- src/chat_server.erl
+++ src/chat_server.erl
@@ -7,7 +7,6 @@
 
 info(#'Message'{from=From,to=To,id=Id}=M, R, S) ->
    kvx:append(M,chat:key({p2p,From,To})),
-   n2o:send({client,From},{flush,M}),
    n2o:send({client,To},{flush,M}),
    {reply,{binary, #'Ack'{id=Id}},R,S};
 
