b3f5dbfdabec19191cdf700d1c37d153ceab8955
Namdak Tonpa <maxim@synrc.com>
2019-04-18 00:01:57 +0300

    cleanup

--- src/chat_server.erl
+++ src/chat_server.erl
@@ -1,8 +1,6 @@
 -module(chat_server).
 -copyright('2014—2019 (c) Synrc Research Center').
 -include("message.hrl").
--include_lib("kvx/include/cursors.hrl").
--include_lib("n2o/include/n2o.hrl").
 -compile(export_all).
 
 info(#'Message'{from=From,to=To,id=Id}=M, R, S) ->
