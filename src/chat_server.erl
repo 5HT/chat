98f4cf1561319d1a1af15cbf02f3a672d7bbbc42
Namdak Tonpa <maxim@synrc.com>
2019-04-25 03:09:00 +0300

    cleanup

--- src/chat_server.erl
+++ src/chat_server.erl
@@ -3,7 +3,7 @@
 -include("message.hrl").
 -compile(export_all).
 
-info(#'Message'{from=From,to=To,id=Id}=Msg, R, S) ->
+info(#'Message'{to=To,id=Id}=Msg, R, S) ->
    kvx:append(Msg,To),
    n2o:send({client,To},{flush,Msg}),
    {reply,{binary, #'Ack'{id=Id}},R,S};
